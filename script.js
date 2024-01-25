2f2da03e2c2c9d48941016eebed7a35fb0aae4f9
haytam <96146752+haytamb@users.noreply.github.com>
2024-01-25 23:05:57 +0100

    Add simple UI

--- script.js
+++ script.js
@@ -1,78 +1,67 @@
+let playerScore = 0;
+let computerScore = 0;
+const btnNodelist = document.querySelectorAll('.choice');
+const outcomeDisplay = document.querySelector('#outcome');
+const scoreDisplay = document.querySelector('#score');
+const resetBtn = document.querySelector('#reset');
+
 function getComputerChoice () {
-    const CHOICE = ['rock', 'paper', 'scissors'];
+    const CHOICE = ['Rock', 'Paper', 'Scissors'];
     return CHOICE[(Math.floor(Math.random() * CHOICE.length))]
 }
 
 function playRound (playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        console.log("It's a tie! Try again.");
-        let playerSelection = prompt('choice?').toLocaleLowerCase()
-        let computerSelection = getComputerChoice()
-        console.log(playerSelection, computerSelection)
-        let result = playRound(playerSelection, computerSelection);
-        return result;
-
-    } else if (playerSelection == 'rock' ) {
-        switch (computerSelection) {
-            case 'scissors':
-                console.log('You win! Rock beats Scissors');
-                return 1;
-            case 'paper':
-                console.log('You Lose! Paper beats Rock');
-                return 0;
-        }
-
-    } else if (playerSelection == 'paper' ) {
-        switch (computerSelection) {
-            case 'rock':
-                console.log('You win! Paper beats Rock');
-                return 1;
-            case 'scissors':
-                console.log('You lose! Scissors beats Paper');
-                return 0;
-        }
-
-    } else if (playerSelection == 'scissors' ) {
-        switch (computerSelection) {
-            case 'paper':
-                console.log('You win! Scissors beats Paper');
-                return 1;
-            case 'rock':
-                console.log('You Lose! Rock beats Scissors');
-                return 0;
-        }
-
-    } else {
-        console.log("Wrong input. Try again.")
-        let playerSelection = prompt('choice?')
-        let computerSelection = getComputerChoice()
-        let result = playRound(playerSelection, computerSelection);
-        return result;
-    }
-}
-
-function game () {
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('choice?').toLocaleLowerCase();
-        let computerSelection = getComputerChoice();
-        console.log(playerSelection, computerSelection)
-        let result = playRound(playerSelection, computerSelection);
-        console.log (result)
-        if (result) {
+    if (playerSelection === computerSelection) {
+        outcomeDisplay.textContent = "It's a tie! Try again.";
+        scoreDisplay.textContent = `Player: ${playerScore} Computer: ${computerScore}`
+    } else if (
+        playerSelection ==='Rock' && computerSelection === 'Scissors' ||
+        playerSelection ==='Paper' && computerSelection === 'Rock' ||
+        playerSelection ==='Scissors' && computerSelection === 'Paper') 
+        {
+            outcomeDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
             playerScore++;
-        } else if (!result) {
+            scoreDisplay.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+            if (playerScore === 5) {
+                outcomeDisplay.textContent = 'Game Over. You won!';
+                btnNodelist.forEach (element => {
+                    element.disabled = true;
+                })
+            }
+        }  else {
+            outcomeDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
             computerScore++;
+            scoreDisplay.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+            if (computerScore === 5) {
+                outcomeDisplay.textContent = 'Game Over. You lost!';
+                btnNodelist.forEach (element => {
+                    element.disabled = true;
+                })
+            }
         }
     }
-    
-    if (playerScore > computerScore) {
-        console.log (`You won the game! Final score: ${playerScore} - ${computerScore}`)
-    } else {
-        console.log(console.log (`You lost the game! Final score: ${playerScore} - ${computerScore}`))
-    }
-}
 
-game()
+btnNodelist.forEach(element => {
+    element.addEventListener('click', () => {
+    const playerSelection = element.textContent;
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    })
+})
+
+resetBtn.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    outcomeDisplay.textContent = '';
+    scoreDisplay.textContent = ''
+    btnNodelist.forEach(element => {element.disabled = false;})
+})
+    
+
+    
+    
+
+
+
+
+
